983e1a2bebc2de538af40807a3a013dfd8f55afe
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-04 22:23:16 +0530

    post

--- first/urls.py
+++ first/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first.views import HelloWorld
+from first.views import HelloWorld,Cars
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',HelloWorld.as_view()),
+    path('cars/',Cars.as_view()),
 ]
--- first/views.py
+++ first/views.py
@@ -5,3 +5,31 @@ class HelloWorld(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+    
+
+
+    def post(self,request):
+        string1=request.data['message1']
+        string2=request.data['message2']
+        content = {'message': string1+string2}
+        return Response(content)
+   
+    
+class Cars(APIView):
+    def get(self,request):
+        description={
+            
+            'car1':{
+                'Model':'Suzuki',
+                'Color':'Black',
+                'Year':'2022',
+            },
+            'car2':{
+                'Model':'Bmw',
+                'Color':'White',
+                'Year':'2021',
+
+            }
+       
+        }  
+        return Response(description)  
