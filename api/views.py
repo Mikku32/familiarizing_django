066b1f818e6a6f9cd64fb332d1c04f0e9559a340
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-05 14:56:24 +0530

    serializer

--- api/views.py
+++ api/views.py
@@ -3,33 +3,50 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from api.models import Student
+from api.serializers import StudentSerializer
 
-# Create your views here.
+
+
+# class StudentListApiview(APIView):
+#     def get(self,request):
+#         students=Student.objects.all() #select * from students
+#         studentList=[] #empty list
+#         for i in students:
+#             studentList.append({
+#                 'id':i.id,
+#                 'name':i.name,
+#                 'roll':i.roll,
+#                 'course':i.course
+#                 })
+#         return Response(studentList)
+    
+
+#using serializer example
 
 class StudentListApiview(APIView):
     def get(self,request):
-        students=Student.objects.all() #select * from students
-        studentList=[] #empty list
-        for i in students:
-            studentList.append({
-                'id':i.id,
-                'name':i.name,
-                'roll':i.roll,
-                'course':i.course
-                })
-        return Response(studentList)
-    
-    
-class StudentDetailApiview(APIView):
-    def get(self,request,pk):
-        students=Student.objects.get(id=pk)
-        return Response({
-            'id':students.id,
-            'name':students.name,
-            'roll':students.roll,
-            'course':students.course
-            })    
+        students=Student.objects.all()
+        stdntserlzr=StudentSerializer(students,many=True)  #many=true because we have multiple students
+        return Response(stdntserlzr.data)
+
+
+
+# class StudentDetailApiview(APIView):
+#     def get(self,request,pk):
+#         students=Student.objects.get(id=pk)
+#         return Response({
+#             'id':students.id,
+#             'name':students.name,
+#             'roll':students.roll,
+#             'course':students.course
+#             })    
     
 
-            
+    #using serializer example
+
+class StudentDetailApiview(APIView):
+    def get(self,request,pk):
+        stud=Student.objects.get(id=pk)  
+        stdntserlzr=StudentSerializer(stud,many=False)
+        return Response(stdntserlzr.data)          
         
